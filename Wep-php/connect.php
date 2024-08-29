ee4e610cc311e57629cf76ad5edbd6e064710fa1
ISRES013 <yeeneiei51@gmail.com>
2024-08-29 10:45:17 +0700

    Add files via upload

---
+++ Wep-php/connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    // แก้ไขการตั้งค่าการเชื่อมต่อ PDO
+    $conn = new PDO("mysql:host=$servername;dbname=regis_db;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected Successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
